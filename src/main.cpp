506aaaa87012a9a69bd0d9e737316ec4b71377d8
ndiahovets <nikolay.diahovets@gmail.com>
2022-05-23 01:28:33 +0300

    initialization and render loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include "log.h"
+#include "vk-engine.h"
 
 int main() {
   log::init();
   log::info("Hello world");
+
+  VulkanEngine engine;
+
+  engine.init();
+
+  engine.run();
+
+  engine.cleanup();
+
   return 0;
 }
