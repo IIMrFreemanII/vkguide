e17e8662d0dd6c451441ebb318475fcd2c3112fd
ndiahovets <nikolay.diahovets@gmail.com>
2022-05-29 17:08:02 +0300

    simplify deletion queue

--- src/vk_engine.h
+++ src/vk_engine.h
@@ -81,13 +81,12 @@ struct DeletionQueue
   std::deque<std::function<void()>> deletors;
 
   void push_function(std::function<void()>&& function) {
-    deletors.push_back(function);
+    deletors.push_front(function);
   }
 
   void flush() {
-    // reverse iterate the deletion queue to execute all the functions
-    for (auto it = deletors.rbegin(); it != deletors.rend(); it++) {
-      (*it)(); //call the function
+    for (const auto& func : deletors) {
+      func();
     }
 
     deletors.clear();
